931284f7636cb5ccef9025a1b429f52229276040
ui2 <phanhongha96@gmail.com>
2022-10-05 23:37:03 +0700

    update hinh anh

---
+++ data/data.js
@@ -0,0 +1,34 @@
+import HinhAnh from "../img/xa_can_cau.png";
+
+export const data = [
+    {
+        id: 1,
+        name: "Ca nấu lẩu, nấu mì mini",
+        shop: "Shop Devang",
+        img: HinhAnh,
+      },
+      {
+        id: 2,
+        name: "1Kg Khô gà bơ tỏi",
+        shop: "Shop Devang",
+        img: HinhAnh,
+      },
+      {
+        id: 3,
+        name: "xe cẩu đa năng",
+        shop: "Shop Devang",
+        img: HinhAnh,
+      },
+      {
+        id: 4,
+        name: "Khô Bò",
+        shop: "Shop Devang",
+        img: HinhAnh,
+      },
+      {
+        id: 5,
+        name: "Khô Gà",
+        shop: "Shop Devang",
+        img: HinhAnh,
+      },
+    ];
